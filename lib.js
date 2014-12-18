0dca6fb56458c108927d94fb511add8b774657ac
Sorawee Porncharoenwase <sporncha@cs.brown.edu>
2014-12-18 16:53:04 -0500

    Test arity

--- lib.js
+++ lib.js
@@ -1,18 +1,21 @@
 define(["d3"], function(d3) {
-  // Arbitrary JS that does what you need here.  You can declare more
-  // dependencies above, as long as you can express them as requirejs modules
-  // and put them in the same directory.  
-
-  function draw(runtime){
-    var detached = document.createElement("div");
-    runtime.getParam("current-animation-port")(detached);
-    detached.innerHTML = "Hello!";
-  }
-  
-  var counter = 0;
-  return {
-    libFunction: function() { return counter++; },
-    draw: draw
-  };
+    // Arbitrary JS that does what you need here.  You can declare more
+    // dependencies above, as long as you can express them as requirejs modules
+    // and put them in the same directory.  
+    
+    function xy_plot(runtime){
+	return function(fun, xleft, xright, yleft, yright, step){
+	    runtime.checkArity(6, arguments, "xy-plot");
+	    var detached = document.createElement("div");
+	    runtime.getParam("current-animation-port")(detached);
+	    detached.innerHTML = "Hello!";
+	}
+    }
+    
+    var counter = 0;
+    return {
+	libFunction: function() { return counter++; },
+	xy_plot: xy_plot,
+    };
 });
 
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var baseUrl =  "https://cs.brown.edu/~sporncha/pyret-d3";
+var baseUrl =  "https://cs.brown.edu/~sporncha/pyret-d3/";
 requirejs.config({
-  paths: {
-    "my-project": baseUrl,
-    d3: "https://cs.brown.edu/~sporncha/d3.v3.min"
-  }
+    paths: {
+	"my-project": baseUrl,
+	d3: baseUrl + "d3.v3.min"
+    }
 });
 requirejs.undef("@js-http/" + baseUrl + "main.js");
 requirejs.undef("my-project/lib");
 define(["js/runtime-util", "my-project/lib"], function(util, lib) {
-  return function(runtime, namespace) {
-    return util.makeModuleReturn(runtime, {}, {
-      "provided-fun": runtime.makeFunction(lib.libFunction),
-      "draw": runtime.makeFunction(function() { return lib.draw(runtime); })
-    });
-  };
+    return function(runtime, namespace) {
+	return util.makeModuleReturn(runtime, {}, {
+	    "provided-fun": runtime.makeFunction(lib.libFunction),
+	    "xy-plot": runtime.makeFunction(lib.xy_plot(runtime))
+	});
+    };
 });
