c968d3b48b5d0e79aef3f03c910be925e82f308e
Sorawee Porncharoenwase <sporncha@cs.brown.edu>
2014-12-18 21:24:01 -0500

    Sketch of graph drawing

---
+++ README.md
@@ -0,0 +1,21 @@
+Usage
+=====
+
+xy-plot
+-------
+Here are arguments
+
+1. a function to be plotted [(Number -> Number)]
+2. x-min [Number]
+3. x-max [Number]
+4. y-min [Number]
+5. y-max [Number]
+5. step [Number]
+
+Example
+=======
+
+xy-plot
+-------
+
+https://patch-experiment.herokuapp.com/editor#share=0Bxr4FfLa3goOYlNjcEl3VVZ4eWM&v=v0.5r763-test
--- lib.js
+++ lib.js
@@ -1,14 +1,43 @@
-define(["d3"], function(d3) {
+define(["d3", "js/js-numbers"], function(d3, jsnums) {
     // Arbitrary JS that does what you need here.  You can declare more
     // dependencies above, as long as you can express them as requirejs modules
     // and put them in the same directory.  
     
     function xy_plot(runtime){
-	return function(fun, xleft, xright, yleft, yright, step){
+	return function(f, x_min, x_max, y_min, y_max, step){
 	    runtime.checkArity(6, arguments, "xy-plot");
-	    var detached = document.createElement("div");
-	    runtime.getParam("current-animation-port")(detached);
-	    detached.innerHTML = "Hello!";
+	    runtime.checkFunction(f);
+	    runtime.checkNumber(x_min);
+	    runtime.checkNumber(x_max);
+	    runtime.checkNumber(y_min);
+	    runtime.checkNumber(y_max);
+	    runtime.checkNumber(step);
+
+	    var max_width = 400;
+	    var max_height = 400;
+	    var s = runtime.makeNumber(100);
+
+	    var data = [];
+	    for(var i = x_min; i <= x_max; i = runtime.plus(i, step)){
+		x = runtime.makeNumber(i)
+		data.push([x, f.app(x)])
+	    }
+	    // flip y-axis to display properly
+	    data = data.map(function(d){ return [d[0], (max_height / s - d[1])]});
+	    var detached = d3.select(document.createElement("div"));
+	    var svg_container = detached.append("svg")
+		                        .attr("width", max_width)
+                      	                .attr("height", max_height);
+	    var circles = svg_container.selectAll("circle")
+	                               .data(data)
+	                               .enter()
+	                               .append("circle");
+	    
+	    var plotted = circles.attr("cx", function (d) { return runtime.times(s, d[0]); })
+                            	 .attr("cy", function (d) { return runtime.times(s, d[1]); })
+	                         .attr("r", 2)
+     	                         .style("fill", "black");
+	    runtime.getParam("current-animation-port")(detached.node());
 	}
     }
     
